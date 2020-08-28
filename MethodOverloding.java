d53967d4611acf6169181325302148cacec48696
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-08-28 08:24:54 +0530

    Update MethodOverloding.java

--- MethodOverloding.java
+++ MethodOverloding.java
@@ -1,21 +1,21 @@
 class MethodOverloding {
-    MethodOverloding() {
-     System.out.println("Constructor of Parent");
+    MethodOverloding () {
+     System.out.println ("Constructor of Parent");
     }
-    void disp() {
-     System.out.println("Parent Method");
+    void disp () {
+     System.out.println ("Parent Method");
     }
  }
  class JavaExample extends MethodOverloding {
-    JavaExample() {
-     System.out.println("Constructor of Child");
+    JavaExample () {
+     System.out.println ("Constructor of Child");
     }
-    void disp() {
-     System.out.println("Child Method");
-     super.disp();
+    void disp () {
+     System.out.println ("Child Method");
+     super.disp ();
     }
-    public static void main(String args[]) {
-     JavaExample obj = new JavaExample();
-     obj.disp();
+    public static void main (String args[]) {
+     JavaExample obj = new JavaExample ();
+     obj.disp ();
     }
  }
